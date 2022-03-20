62f3b379042bd3cadc165ba59986fa4571b1f976
NatalieCK <270053597@yoobeestudent.ac.nz>
2022-03-21 08:59:54 +1300

    Update NatsComponent.vue

---
+++ src/components/NatsComponent.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div class="nats">
+    <h1>Hello this is Nats Component</h1></div>
+</template>
+
+<style scoped>
+
+.nats {
+    background-color: blue;
+    color: cornflowerblue;
+    width: 100vw;
+    height: 200px;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+
+</style>
