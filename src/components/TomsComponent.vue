ea4e4b6c7b41f745051fc2785a36c9851a8bd25c
TheRealTomWilliamson <270153381@yoobeestudent.ac.nz>
2022-03-21 10:05:40 +1300

    updates

--- src/components/TomsComponent.vue
+++ src/components/TomsComponent.vue
@@ -1,4 +1,20 @@
 
 <template>
+<div class="egg">
 <h1>Why did the chicken write some code?</h1>
+</div>
 </template>
+
+<style scoped>
+
+.egg{
+    display: flex;
+    justify-content: center;
+}
+h1{
+    color: yellow;
+    padding: 2px 12px;
+    background-color: #999;
+    display: inline-block;
+}
+</style>
