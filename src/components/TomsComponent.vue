61e2c898e9ebe3b44d6c987b6c2ff29a982b839e
TheRealTomWilliamson <270153381@yoobeestudent.ac.nz>
2022-03-21 10:13:01 +1300

    Update TomsComponent.vue

--- src/components/TomsComponent.vue
+++ src/components/TomsComponent.vue
@@ -2,6 +2,7 @@
 <template>
 <div class="egg">
 <h1>Why did the chicken write some code?</h1>
+<img src="src/assets/eskmjied19h61.jpg" alt="chicken">
 </div>
 </template>
 
@@ -9,12 +10,16 @@
 
 .egg{
     display: flex;
-    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 }
 h1{
     color: yellow;
     padding: 2px 12px;
     background-color: #999;
     display: inline-block;
+}
+img{
+    width: 500px;
 }
 </style>
